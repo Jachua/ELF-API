d8875425f635fb9480dfa46c961354a5845923fc
XINDONG <jacquelinemxh@yahoo.com>
2018-07-30 14:45:06 +0800

    dummy

--- client/elf.py
+++ client/elf.py
@@ -1,5 +1,54 @@
 
 
+# import grpc
+
+# import play_pb2
+# import play_pb2_grpc
+
+# from server_addrs import addrs
+
+
+# def get_move():
+#     x = int(input("Enter coordiante x \n>"))
+#     y = int(input("Enter coorindate y \n>"))
+#     return x, y
+
+# def run():
+#     address = addrs["game_server"]
+#     if address != "":
+#         channel = grpc.insecure_channel(address + ":50051")
+#     else: 
+#         channel = grpc.insecure_channel('localhost:50051')
+#     stub = play_pb2_grpc.TurnStub(channel)
+#     if input("Enter 'B' for Black, 'W' for White \n>").upper() == "B":
+#         human_color = 1
+#     else:
+#         human_color = 2
+#     # ID_state = stub.GetID(play_pb2.State(status = True, ID = ""))
+#     # while not ID_state.status:
+#     #     print("Waiting for AI to connect to server...")
+#     #     ID_state = stub.GetID(play_pb2.State(status = True, ID = ""))
+#     # ID = ID_state.ID
+#     id = ""
+#     _ = stub.SetPlayer(play_pb2.Player(color = human_color, ID = id))
+#     AI_color = human_color % 2 + 1
+#     while True: 
+#         if stub.IsNextPlayer(play_pb2.Player(color = human_color, ID = id)).status:
+#             x, y = get_move()
+#             _ = stub.SetMove(play_pb2.Step(x = x, y = y, player = play_pb2.Player(color =  human_color, ID = id)))
+#             _ = stub.UpdateNext(play_pb2.State(status = True, ID = id))
+#             human_move = stub.GetMove(play_pb2.Player(color = human_color, ID = id))
+#             print("You placed a stone at coordinates: ", human_move.x, ",", human_move.y)
+#         else: 
+#             while stub.IsNextPlayer(play_pb2.Player(color = AI_color, ID = id)).status:
+#                 pass
+#             AI_move = stub.GetMove(play_pb2.Player(color = AI_color, ID = id))
+#             # print("OpenGo placed a stone at coordinates: ", AI_move.x, ",", AI_move.y)
+
+
+# if __name__ == '__main__':
+#     run()
+
 import grpc
 
 import play_pb2
@@ -20,31 +69,25 @@ def run():
     else: 
         channel = grpc.insecure_channel('localhost:50051')
     stub = play_pb2_grpc.TurnStub(channel)
-    # ID = int(input("Enter a numerical ID to start the game \n>"))
-    # _ = stub.NewRoom(play_pb2.State(status = True, ID = ID))
     if input("Enter 'B' for Black, 'W' for White \n>").upper() == "B":
         human_color = 1
     else:
         human_color = 2
-    ID_state = stub.GetID(play_pb2.State(status = True, ID = ""))
-    while not ID_state.status:
-        print("Waiting for AI to connect to server...")
-        ID_state = stub.GetID(play_pb2.State(status = True, ID = ""))
-    ID = ID_state.ID
-    _ = stub.SetPlayer(play_pb2.Player(color = human_color, ID = ID))
+    _ = stub.SetPlayer(play_pb2.Player(color = human_color))
     AI_color = human_color % 2 + 1
     while True: 
-        if stub.IsNextPlayer(play_pb2.Player(color = human_color, ID = ID)).status:
+        if stub.IsNextPlayer(play_pb2.Player(color = human_color)).status:
             x, y = get_move()
-            _ = stub.SetMove(play_pb2.Step(x = x, y = y, player = play_pb2.Player(color =  human_color, ID = ID)))
-            _ = stub.UpdateNext(play_pb2.State(status = True, ID = ID))
-            human_move = stub.GetMove(play_pb2.Player(color = human_color, ID = ID))
+           
+            _ = stub.SetMove(play_pb2.Step(x = x, y = y, player = play_pb2.Player(color =  human_color)))
+            _ = stub.UpdateNext(play_pb2.State(status = True))
+            human_move = stub.GetMove(play_pb2.Player(color = human_color))
             print("You placed a stone at coordinates: ", human_move.x, ",", human_move.y)
         else: 
-            while stub.IsNextPlayer(play_pb2.Player(color = AI_color, ID = ID)).status:
+            while stub.IsNextPlayer(play_pb2.Player(color = AI_color)).status:
                 pass
-            AI_move = stub.GetMove(play_pb2.Player(color = AI_color, ID = ID))
-            print("OpenGo placed a stone at coordinates: ", AI_move.x, ",", AI_move.y)
+            AI_move = stub.GetMove(play_pb2.Player(color = AI_color))
+            print("AI places a stone at coordinates: ", AI_move.x, ",", AI_move.y)
 
 
 if __name__ == '__main__':
--- server/server.go
+++ server/server.go
@@ -1,11 +1,202 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net"
+
+// 	pb "ELF-API"
+
+// 	"golang.org/x/net/context"
+// 	"google.golang.org/grpc"
+// 	"google.golang.org/grpc/reflection"
+// )
+
+// const (
+// 	port = ":50051"
+// )
+
+// //color = 1 Black
+// //color = 2 White
+// type player struct {
+// 	prevMove *pb.Step
+// 	color    uint32
+// 	hasMoved bool
+// 	ID       string
+// }
+
+// type room struct {
+// 	human      *player
+// 	AI         *player
+// 	nextPlayer uint32
+// 	hasChosen  bool
+// 	ID         string
+// 	isAssigned bool
+// }
+
+// type server struct {
+// 	rooms map[string]*room
+// }
+
+// func (r *room) InRoom(p *player) bool {
+// 	return r.ID == p.ID
+// }
+
+// func (s *server) GetRoom(id string) *room {
+// 	return s.rooms[id]
+// }
+
+// func (r *room) IsHuman(in *pb.Player) bool {
+// 	if in.Color == r.human.color {
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func checkNil(in *pb.Step) *pb.Step {
+// 	switch in {
+// 	case nil:
+// 		return &pb.Step{X: 20, Y: 20, Player: &pb.Player{Color: 3, ID: ""}}
+// 	default:
+// 		return in
+// 	}
+// }
+
+// func (s *server) GetMove(ctx context.Context, in *pb.Player) (*pb.Step, error) {
+// 	r := s.GetRoom(in.ID)
+// 	if r.IsHuman(in) {
+// 		return checkNil(r.human.prevMove), nil
+// 	}
+// 	return checkNil(r.AI.prevMove), nil
+// }
+
+// func (s *server) SetMove(ctx context.Context, in *pb.Step) (*pb.State, error) {
+// 	log.Println("Registering move from next player...")
+// 	r := s.GetRoom(in.Player.ID)
+// 	if r.IsHuman(in.Player) {
+// 		r.human.prevMove = in
+// 		r.human.hasMoved = true
+// 	} else {
+// 		r.AI.prevMove = in
+// 		r.AI.hasMoved = true
+// 	}
+// 	return &pb.State{Status: true, ID: r.ID}, nil
+// }
+
+// func (s *server) HasMoved(ctx context.Context, in *pb.Player) (*pb.State, error) {
+// 	r := s.GetRoom(in.ID)
+// 	if r.IsHuman(in) {
+// 		return &pb.State{Status: r.human.hasMoved, ID: r.ID}, nil
+// 	}
+// 	return &pb.State{Status: r.AI.hasMoved, ID: r.ID}, nil
+// }
+
+// func (s *server) UpdateNext(ctx context.Context, in *pb.State) (*pb.State, error) {
+// 	r := s.GetRoom(in.ID)
+// 	r.nextPlayer = r.nextPlayer%2 + 1
+// 	r.human.hasMoved = false
+// 	r.AI.hasMoved = false
+// 	return &pb.State{Status: true, ID: in.ID}, nil
+// }
+
+// func (s *server) IsNextPlayer(ctx context.Context, in *pb.Player) (*pb.State, error) {
+// 	r := s.GetRoom(in.ID)
+// 	return &pb.State{Status: r.nextPlayer == in.Color, ID: r.ID}, nil
+// }
+
+// func (s *server) SetPlayer(ctx context.Context, in *pb.Player) (*pb.State, error) {
+// 	r := s.GetRoom(in.ID)
+// 	log.Println("The ID of the room is: ", r.ID)
+// 	r.human.color = in.Color
+// 	r.AI.color = in.Color%2 + 1
+// 	r.hasChosen = true
+// 	return &pb.State{Status: true, ID: r.ID}, nil
+// }
+
+// func (s *server) GetAIPlayer(ctx context.Context, in *pb.State) (*pb.Player, error) {
+// 	r := s.GetRoom(in.ID)
+// 	return &pb.Player{Color: r.AI.color, ID: r.ID}, nil
+// }
+
+// func (s *server) HasChosen(ctx context.Context, in *pb.State) (*pb.State, error) {
+// 	r := s.GetRoom(in.ID)
+// 	return &pb.State{Status: r.hasChosen, ID: r.ID}, nil
+// }
+
+// func (s *server) NewRoom(ctx context.Context, in *pb.State) (*pb.State, error) {
+// 	// u, err := uuid.NewV4()
+// 	// if err != nil {
+// 	// 	fmt.Println("Error: ", err)
+// 	// 	return &pb.State{Status: false, ID: ""}, err
+// 	// }
+// 	// id := u.String()
+// 	id := ""
+// 	s.rooms[id] = &room{
+// 		human: &player{
+// 			prevMove: nil,
+// 			hasMoved: false,
+// 			color:    1,
+// 			ID:       id,
+// 		},
+// 		AI: &player{
+// 			prevMove: nil,
+// 			hasMoved: false,
+// 			color:    2,
+// 			ID:       id,
+// 		},
+// 		ID:         id,
+// 		isAssigned: false,
+// 		nextPlayer: 1,
+// 		hasChosen:  false,
+// 	}
+// 	return &pb.State{Status: true, ID: id}, nil
+// }
+
+// func (s *server) GetID(ctx context.Context, in *pb.State) (*pb.State, error) {
+// 	for id, room := range s.rooms {
+// 		if !room.isAssigned {
+// 			room.isAssigned = true
+// 			return &pb.State{Status: true, ID: id}, nil
+// 		}
+// 	}
+// 	log.Println("Failed to assign a room to player.")
+// 	return &pb.State{Status: false, ID: ""}, nil
+// }
+
+// func main() {
+// 	lis, err := net.Listen("tcp", port)
+// 	if err != nil {
+// 		log.Fatalf("failed to listen: %v", err)
+// 	}
+// 	s := grpc.NewServer()
+// 	// playserver := &server{
+// 	// 	human: &player{
+// 	// 		prevMove: nil,
+// 	// 		hasMoved: false,
+// 	// 		color:    1,
+// 	// 	},
+// 	// 	AI: &player{
+// 	// 		prevMove: nil,
+// 	// 		hasMoved: false,
+// 	// 		color:    2,
+// 	// 	},
+// 	// 	nextPlayer: 1,
+// 	// 	hasChosen:  false,
+// 	// }
+// 	gameServer := &server{
+// 		rooms: make(map[string]*room),
+// 	}
+// 	pb.RegisterTurnServer(s, gameServer)
+// 	// Register reflection service on gRPC server.
+// 	reflection.Register(s)
+// 	if err := s.Serve(lis); err != nil {
+// 		log.Fatalf("failed to serve: %v", err)
+// 	}
+// }
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
-
-	"github.com/nu7hatch/gouuid"
 
 	pb "ELF-API"
 
@@ -24,32 +215,17 @@ type player struct {
 	prevMove *pb.Step
 	color    uint32
 	hasMoved bool
-	ID       string
 }
 
-type room struct {
+type server struct {
 	human      *player
 	AI         *player
 	nextPlayer uint32
 	hasChosen  bool
-	ID         string
-	isAssigned bool
 }
 
-type server struct {
-	rooms map[string]*room
-}
-
-func (r *room) InRoom(p *player) bool {
-	return r.ID == p.ID
-}
-
-func (s *server) GetRoom(id string) *room {
-	return s.rooms[id]
-}
-
-func (r *room) IsHuman(in *pb.Player) bool {
-	if in.Color == r.human.color {
+func (s *server) IsHuman(in *pb.Player) bool {
+	if in.Color == s.human.color {
 		return true
 	}
 	return false
@@ -58,107 +234,71 @@ func (r *room) IsHuman(in *pb.Player) bool {
 func checkNil(in *pb.Step) *pb.Step {
 	switch in {
 	case nil:
-		return &pb.Step{X: 20, Y: 20, Player: &pb.Player{Color: 3, ID: ""}}
+		return &pb.Step{X: 20, Y: 20, Player: &pb.Player{Color: 3}}
 	default:
 		return in
 	}
 }
 
 func (s *server) GetMove(ctx context.Context, in *pb.Player) (*pb.Step, error) {
-	r := s.GetRoom(in.ID)
-	if r.IsHuman(in) {
-		return checkNil(r.human.prevMove), nil
+
+	if s.IsHuman(in) {
+		return checkNil(s.human.prevMove), nil
 	}
-	return checkNil(r.AI.prevMove), nil
+	return checkNil(s.AI.prevMove), nil
 }
 
 func (s *server) SetMove(ctx context.Context, in *pb.Step) (*pb.State, error) {
 	log.Println("Registering move from next player...")
-	r := s.GetRoom(in.Player.ID)
-	if r.IsHuman(in.Player) {
-		r.human.prevMove = in
-		r.human.hasMoved = true
+	if s.IsHuman(in.Player) {
+		s.human.prevMove = in
+		s.human.hasMoved = true
 	} else {
-		r.AI.prevMove = in
-		r.AI.hasMoved = true
+		s.AI.prevMove = in
+		s.AI.hasMoved = true
 	}
-	return &pb.State{Status: true, ID: r.ID}, nil
+	return &pb.State{Status: true}, nil
 }
 
 func (s *server) HasMoved(ctx context.Context, in *pb.Player) (*pb.State, error) {
-	r := s.GetRoom(in.ID)
-	if r.IsHuman(in) {
-		return &pb.State{Status: r.human.hasMoved, ID: r.ID}, nil
+	if s.IsHuman(in) {
+		return &pb.State{Status: s.human.hasMoved}, nil
 	}
-	return &pb.State{Status: r.AI.hasMoved, ID: r.ID}, nil
+	return &pb.State{Status: s.AI.hasMoved}, nil
 }
 
 func (s *server) UpdateNext(ctx context.Context, in *pb.State) (*pb.State, error) {
-	r := s.GetRoom(in.ID)
-	r.nextPlayer = r.nextPlayer%2 + 1
-	r.human.hasMoved = false
-	r.AI.hasMoved = false
-	return &pb.State{Status: true, ID: in.ID}, nil
+	s.nextPlayer = s.nextPlayer%2 + 1
+	s.human.hasMoved = false
+	s.AI.hasMoved = false
+	return &pb.State{Status: true}, nil
 }
 
 func (s *server) IsNextPlayer(ctx context.Context, in *pb.Player) (*pb.State, error) {
-	r := s.GetRoom(in.ID)
-	return &pb.State{Status: r.nextPlayer == in.Color, ID: r.ID}, nil
+	return &pb.State{Status: s.nextPlayer == in.Color}, nil
 }
 
 func (s *server) SetPlayer(ctx context.Context, in *pb.Player) (*pb.State, error) {
-	r := s.GetRoom(in.ID)
-	r.human.color = in.Color
-	r.AI.color = in.Color%2 + 1
-	r.hasChosen = true
-	return &pb.State{Status: true, ID: r.ID}, nil
+	s.human.color = in.Color
+	s.AI.color = in.Color%2 + 1
+	s.hasChosen = true
+	return &pb.State{Status: true}, nil
 }
 
 func (s *server) GetAIPlayer(ctx context.Context, in *pb.State) (*pb.Player, error) {
-	r := s.GetRoom(in.ID)
-	return &pb.Player{Color: r.AI.color, ID: r.ID}, nil
+	return &pb.Player{Color: s.AI.color}, nil
 }
 
 func (s *server) HasChosen(ctx context.Context, in *pb.State) (*pb.State, error) {
-	r := s.GetRoom(in.ID)
-	return &pb.State{Status: r.hasChosen, ID: r.ID}, nil
+	return &pb.State{Status: s.hasChosen}, nil
 }
 
 func (s *server) NewRoom(ctx context.Context, in *pb.State) (*pb.State, error) {
-	u, err := uuid.NewV4()
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return &pb.State{Status: false, ID: ""}, err
-	}
-	id := u.String()
-	s.rooms[id] = &room{
-		human: &player{
-			prevMove: nil,
-			hasMoved: false,
-			color:    1,
-			ID:       id,
-		},
-		AI: &player{
-			prevMove: nil,
-			hasMoved: false,
-			color:    2,
-			ID:       id,
-		},
-		ID:         id,
-		isAssigned: false,
-	}
-	return &pb.State{Status: true, ID: id}, nil
+	return in, nil
 }
 
 func (s *server) GetID(ctx context.Context, in *pb.State) (*pb.State, error) {
-	for id, room := range s.rooms {
-		if !room.isAssigned {
-			room.isAssigned = true
-			return &pb.State{Status: true, ID: id}, nil
-		}
-	}
-	log.Println("Failed to assign a room to player.")
-	return &pb.State{Status: false, ID: ""}, nil
+	return in, nil
 }
 
 func main() {
@@ -167,24 +307,21 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	// playserver := &server{
-	// 	human: &player{
-	// 		prevMove: nil,
-	// 		hasMoved: false,
-	// 		color:    1,
-	// 	},
-	// 	AI: &player{
-	// 		prevMove: nil,
-	// 		hasMoved: false,
-	// 		color:    2,
-	// 	},
-	// 	nextPlayer: 1,
-	// 	hasChosen:  false,
-	// }
-	gameServer := &server{
-		rooms: make(map[string]*room),
+	playServer := &server{
+		human: &player{
+			prevMove: nil,
+			hasMoved: false,
+			color:    1,
+		},
+		AI: &player{
+			prevMove: nil,
+			hasMoved: false,
+			color:    2,
+		},
+		nextPlayer: 1,
+		hasChosen:  false,
 	}
-	pb.RegisterTurnServer(s, gameServer)
+	pb.RegisterTurnServer(s, playServer)
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
