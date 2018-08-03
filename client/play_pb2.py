5d5ae7f225f22907aad48c9a1a273960f7827135
黄梦曦 <jacquelinemxh@yahoo.com>
2018-08-03 17:12:30 +0800

    update grpc

--- client/play_pb2.py
+++ client/play_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='play.proto',
   package='play',
   syntax='proto3',
-  serialized_pb=_b('\n\nplay.proto\x12\x04play\":\n\x04Step\x12\t\n\x01x\x18\x01 \x01(\x11\x12\t\n\x01y\x18\x02 \x01(\x11\x12\x1c\n\x06player\x18\x03 \x01(\x0b\x32\x0c.play.Player\"#\n\x06Player\x12\r\n\x05\x63olor\x18\x01 \x01(\r\x12\n\n\x02ID\x18\x02 \x01(\t\"#\n\x05State\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\n\n\x02ID\x18\x02 \x01(\t\"#\n\x07Resumed\x12\x0c\n\x04move\x18\x01 \x03(\t\x12\n\n\x02ID\x18\x02 \x01(\t2\x98\x05\n\x04Turn\x12$\n\x07SetMove\x12\n.play.Step\x1a\x0b.play.State\"\x00\x12%\n\x07GetMove\x12\x0c.play.Player\x1a\n.play.Step\"\x00\x12(\n\nUpdateNext\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12+\n\x0cIsNextPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12(\n\tSetPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\x0bGetAIPlayer\x12\x0b.play.State\x1a\x0c.play.Player\"\x00\x12\'\n\tHasChosen\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\x08HasMoved\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\nSetResumed\x12\r.play.Resumed\x1a\x0b.play.State\"\x00\x12*\n\nGetResumed\x12\x0b.play.State\x1a\r.play.Resumed\"\x00\x12%\n\x07NewRoom\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12#\n\x05GetID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12%\n\x07SetExit\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tCheckExit\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tSetUserID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tGetUserID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x62\x06proto3')
+  serialized_pb=_b('\n\nplay.proto\x12\x04play\":\n\x04Step\x12\t\n\x01x\x18\x01 \x01(\x11\x12\t\n\x01y\x18\x02 \x01(\x11\x12\x1c\n\x06player\x18\x03 \x01(\x0b\x32\x0c.play.Player\"#\n\x06Player\x12\r\n\x05\x63olor\x18\x01 \x01(\r\x12\n\n\x02ID\x18\x02 \x01(\t\"3\n\x05State\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\n\n\x02ID\x18\x02 \x01(\t\x12\x0e\n\x06userID\x18\x03 \x01(\t\"#\n\x07Resumed\x12\x0c\n\x04move\x18\x01 \x03(\t\x12\n\n\x02ID\x18\x02 \x01(\t2\x98\x05\n\x04Turn\x12$\n\x07SetMove\x12\n.play.Step\x1a\x0b.play.State\"\x00\x12%\n\x07GetMove\x12\x0c.play.Player\x1a\n.play.Step\"\x00\x12(\n\nUpdateNext\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12+\n\x0cIsNextPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12(\n\tSetPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\x0bGetAIPlayer\x12\x0b.play.State\x1a\x0c.play.Player\"\x00\x12\'\n\tHasChosen\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\x08HasMoved\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\nSetResumed\x12\r.play.Resumed\x1a\x0b.play.State\"\x00\x12*\n\nGetResumed\x12\x0b.play.State\x1a\r.play.Resumed\"\x00\x12%\n\x07NewRoom\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12#\n\x05GetID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12%\n\x07SetExit\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tCheckExit\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tSetUserID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tGetUserID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x62\x06proto3')
 )
 
 
@@ -129,6 +129,13 @@ _STATE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='userID', full_name='play.State.userID', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -142,7 +149,7 @@ _STATE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=117,
-  serialized_end=152,
+  serialized_end=168,
 )
 
 
@@ -179,8 +186,8 @@ _RESUMED = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=154,
-  serialized_end=189,
+  serialized_start=170,
+  serialized_end=205,
 )
 
 _STEP.fields_by_name['player'].message_type = _PLAYER
@@ -226,8 +233,8 @@ _TURN = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   options=None,
-  serialized_start=192,
-  serialized_end=856,
+  serialized_start=208,
+  serialized_end=872,
   methods=[
   _descriptor.MethodDescriptor(
     name='SetMove',
