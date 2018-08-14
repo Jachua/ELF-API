68f25194bc9d9b54d46f5d32ea8f15f78e10b54e
黄梦曦 <jacquelinemxh@yahoo.com>
2018-08-14 11:27:22 +0800

    update grpc

--- client/play_pb2.py
+++ client/play_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='play.proto',
   package='play',
   syntax='proto3',
-  serialized_pb=_b('\n\nplay.proto\x12\x04play\"I\n\x04Step\x12\t\n\x01x\x18\x01 \x01(\x11\x12\t\n\x01y\x18\x02 \x01(\x11\x12\x1c\n\x06player\x18\x03 \x01(\x0b\x32\x0c.play.Player\x12\r\n\x05score\x18\x04 \x01(\t\"#\n\x06Player\x12\r\n\x05\x63olor\x18\x01 \x01(\r\x12\n\n\x02ID\x18\x02 \x01(\t\"B\n\x05State\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\n\n\x02ID\x18\x02 \x01(\t\x12\x0e\n\x06userID\x18\x03 \x01(\t\x12\r\n\x05score\x18\x04 \x01(\t\"#\n\x07Resumed\x12\x0c\n\x04move\x18\x01 \x03(\t\x12\n\n\x02ID\x18\x02 \x01(\t2\xc6\x04\n\x04Turn\x12$\n\x07SetMove\x12\n.play.Step\x1a\x0b.play.State\"\x00\x12%\n\x07GetMove\x12\x0c.play.Player\x1a\n.play.Step\"\x00\x12(\n\nUpdateNext\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12+\n\x0cIsNextPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12(\n\tSetPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\x0bGetAIPlayer\x12\x0b.play.State\x1a\x0c.play.Player\"\x00\x12\'\n\tHasChosen\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\x08HasMoved\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\nSetResumed\x12\r.play.Resumed\x1a\x0b.play.State\"\x00\x12*\n\nGetResumed\x12\x0b.play.State\x1a\r.play.Resumed\"\x00\x12%\n\x07NewRoom\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12#\n\x05GetID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12%\n\x07SetExit\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tCheckExit\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x62\x06proto3')
+  serialized_pb=_b('\n\nplay.proto\x12\x04play\":\n\x04Step\x12\t\n\x01x\x18\x01 \x01(\x11\x12\t\n\x01y\x18\x02 \x01(\x11\x12\x1c\n\x06player\x18\x03 \x01(\x0b\x32\x0c.play.Player\"#\n\x06Player\x12\r\n\x05\x63olor\x18\x01 \x01(\r\x12\n\n\x02ID\x18\x02 \x01(\t\"3\n\x05State\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\n\n\x02ID\x18\x02 \x01(\t\x12\x0e\n\x06userID\x18\x03 \x01(\t\"#\n\x07Resumed\x12\x0c\n\x04move\x18\x01 \x03(\t\x12\n\n\x02ID\x18\x02 \x01(\t\"#\n\x05Score\x12\x0e\n\x06result\x18\x01 \x01(\t\x12\n\n\x02ID\x18\x02 \x01(\t2\xed\x04\n\x04Turn\x12$\n\x07SetMove\x12\n.play.Step\x1a\x0b.play.State\"\x00\x12%\n\x07GetMove\x12\x0c.play.Player\x1a\n.play.Step\"\x00\x12(\n\nUpdateNext\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12+\n\x0cIsNextPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12(\n\tSetPlayer\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\x0bGetAIPlayer\x12\x0b.play.State\x1a\x0c.play.Player\"\x00\x12\'\n\tHasChosen\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\x08HasMoved\x12\x0c.play.Player\x1a\x0b.play.State\"\x00\x12*\n\nSetResumed\x12\r.play.Resumed\x1a\x0b.play.State\"\x00\x12*\n\nGetResumed\x12\x0b.play.State\x1a\r.play.Resumed\"\x00\x12%\n\x07NewRoom\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12#\n\x05GetID\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12%\n\x07SetExit\x12\x0b.play.State\x1a\x0b.play.State\"\x00\x12\'\n\tCheckExit\x12\x0b.play.Score\x1a\x0b.play.State\"\x00\x12%\n\x07GetExit\x12\x0b.play.State\x1a\x0b.play.Score\"\x00\x62\x06proto3')
 )
 
 
@@ -53,13 +53,6 @@ _STEP = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='score', full_name='play.Step.score', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -73,7 +66,7 @@ _STEP = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=20,
-  serialized_end=93,
+  serialized_end=78,
 )
 
 
@@ -110,8 +103,8 @@ _PLAYER = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=95,
-  serialized_end=130,
+  serialized_start=80,
+  serialized_end=115,
 )
 
 
@@ -143,13 +136,6 @@ _STATE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='score', full_name='play.State.score', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -162,8 +148,8 @@ _STATE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=132,
-  serialized_end=198,
+  serialized_start=117,
+  serialized_end=168,
 )
 
 
@@ -200,8 +186,46 @@ _RESUMED = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=200,
-  serialized_end=235,
+  serialized_start=170,
+  serialized_end=205,
+)
+
+
+_SCORE = _descriptor.Descriptor(
+  name='Score',
+  full_name='play.Score',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='result', full_name='play.Score.result', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='ID', full_name='play.Score.ID', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=207,
+  serialized_end=242,
 )
 
 _STEP.fields_by_name['player'].message_type = _PLAYER
@@ -209,6 +233,7 @@ DESCRIPTOR.message_types_by_name['Step'] = _STEP
 DESCRIPTOR.message_types_by_name['Player'] = _PLAYER
 DESCRIPTOR.message_types_by_name['State'] = _STATE
 DESCRIPTOR.message_types_by_name['Resumed'] = _RESUMED
+DESCRIPTOR.message_types_by_name['Score'] = _SCORE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 Step = _reflection.GeneratedProtocolMessageType('Step', (_message.Message,), dict(
@@ -239,6 +264,13 @@ Resumed = _reflection.GeneratedProtocolMessageType('Resumed', (_message.Message,
   ))
 _sym_db.RegisterMessage(Resumed)
 
+Score = _reflection.GeneratedProtocolMessageType('Score', (_message.Message,), dict(
+  DESCRIPTOR = _SCORE,
+  __module__ = 'play_pb2'
+  # @@protoc_insertion_point(class_scope:play.Score)
+  ))
+_sym_db.RegisterMessage(Score)
+
 
 
 _TURN = _descriptor.ServiceDescriptor(
@@ -247,8 +279,8 @@ _TURN = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   options=None,
-  serialized_start=238,
-  serialized_end=820,
+  serialized_start=245,
+  serialized_end=866,
   methods=[
   _descriptor.MethodDescriptor(
     name='SetMove',
@@ -372,8 +404,17 @@ _TURN = _descriptor.ServiceDescriptor(
     full_name='play.Turn.CheckExit',
     index=13,
     containing_service=None,
-    input_type=_STATE,
+    input_type=_SCORE,
     output_type=_STATE,
+    options=None,
+  ),
+  _descriptor.MethodDescriptor(
+    name='GetExit',
+    full_name='play.Turn.GetExit',
+    index=14,
+    containing_service=None,
+    input_type=_STATE,
+    output_type=_SCORE,
     options=None,
   ),
 ])
--- client/play_pb2_grpc.py
+++ client/play_pb2_grpc.py
@@ -81,8 +81,13 @@ class TurnStub(object):
         )
     self.CheckExit = channel.unary_unary(
         '/play.Turn/CheckExit',
-        request_serializer=play__pb2.State.SerializeToString,
+        request_serializer=play__pb2.Score.SerializeToString,
         response_deserializer=play__pb2.State.FromString,
+        )
+    self.GetExit = channel.unary_unary(
+        '/play.Turn/GetExit',
+        request_serializer=play__pb2.State.SerializeToString,
+        response_deserializer=play__pb2.Score.FromString,
         )
 
 
@@ -188,6 +193,13 @@ class TurnServicer(object):
     context.set_details('Method not implemented!')
     raise NotImplementedError('Method not implemented!')
 
+  def GetExit(self, request, context):
+    # missing associated documentation comment in .proto file
+    pass
+    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+    context.set_details('Method not implemented!')
+    raise NotImplementedError('Method not implemented!')
+
 
 def add_TurnServicer_to_server(servicer, server):
   rpc_method_handlers = {
@@ -258,8 +270,13 @@ def add_TurnServicer_to_server(servicer, server):
       ),
       'CheckExit': grpc.unary_unary_rpc_method_handler(
           servicer.CheckExit,
-          request_deserializer=play__pb2.State.FromString,
+          request_deserializer=play__pb2.Score.FromString,
           response_serializer=play__pb2.State.SerializeToString,
+      ),
+      'GetExit': grpc.unary_unary_rpc_method_handler(
+          servicer.GetExit,
+          request_deserializer=play__pb2.State.FromString,
+          response_serializer=play__pb2.Score.SerializeToString,
       ),
   }
   generic_handler = grpc.method_handlers_generic_handler(
